070ccd509a76e7279e9ec306c43770b38616d04a
mason <mpike21797@gmail.com>
2022-11-01 19:53:03 +0100

    cleaning

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.example.diceroller
 
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -35,10 +30,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
+    DiceWithButtonAndImage(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
 }
 
 @Preview(showBackground = true)
@@ -54,10 +46,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
             Image(painterResource(imageResource), contentDescription = result.toString())
             Spacer(modifier = Modifier.height(16.dp))
             Button(onClick = { result = (1..6).random() }) {
